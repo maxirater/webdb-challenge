942dd45b25ff7e9921dec807905ff65b15c3524e
Brad Cantrell <brad777mx@hotmail.com>
2019-05-10 10:36:33 -0700

    finished, cant get project/action data to format properly

--- projects/projects-model.js
+++ projects/projects-model.js
@@ -12,11 +12,39 @@ function find() {
     return db("projects")
 }
 
+// function findById(id) {
+//     return db("projects")
+//         .where({ id })
+//         .first()
+// }
+
 function findById(id) {
     return db("projects")
-        .where({ id })
-        .first()
+        .join("actions", "actions.project_id", "projects.id")
+        // .select("projects.id", "actions.description")
+        .where("actions.project_id", id)
+        // .first()
 }
+
+// function findById(id) {
+//     db("projects")
+//         .where({ id })
+//         .first()
+//         .then(proj => {
+//             const theProj = proj
+//         })
+//         .catch(err => {console.log(err)})
+
+//     db('actions')
+    
+    
+//     const actionsArray = theActions.filter(x => {
+//         return x.project_id == id
+//     })
+
+//     theProj.actions = actionsArray
+//     return theProject
+// }
 
 function add(project) {
     // passing 'id' as the second Param is recommended to ensure the id is returned
